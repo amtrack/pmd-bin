48eacfcc5ac58cac3c4ac417d61c3038a2f5c9c8
Matthias Rolke <mr.amtrack@gmail.com>
2022-11-08 21:38:46 +0100

    chore(CI): GH Workflow to update PMD version

---
+++ scripts/update-pmd-version.mjs
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+import { readFile, writeFile } from "node:fs/promises";
+
+main().catch((e) => {
+  console.error(e.message);
+  process.exitCode = 1;
+});
+
+async function main() {
+  // STDIN
+  const latestPmdVersion = await getLatestPmdVersion();
+  console.log(latestPmdVersion);
+  const pjson = JSON.parse(await readFile("package.json", "utf8"));
+  pjson.pmd.version = latestPmdVersion;
+  await writeFile("package.json", JSON.stringify(pjson, null, 2) + "\n");
+}
+
+async function getLatestPmdVersion() {
+  const response = await fetch(
+    "https://api.github.com/repos/pmd/pmd/git/matching-refs/tags/pmd_releases"
+  );
+  const releaseTags = await response.json();
+
+  const versions = releaseTags
+    .map((releaseTag) => releaseTag.ref.split("/").at(-1))
+    // it already seems to be sorted but just in case
+    .sort(compareSemanticVersions);
+  return versions.at(-1);
+}
+
+function compareSemanticVersions(a, b) {
+  return a.localeCompare(b, undefined, {
+    numeric: true,
+    sensitivity: "base",
+  });
+}
